2492cbab67a911dd9ebad3f6c42dc1b14ef4063c
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-29 15:37:36 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
+
 const App = () => {
-  
+  const[marcoPolo, setMarcoPolo] = useState("Marco");
+  const[marcoPoloToggler, setMarcoPoloToggler] = useState("Polo");
+
+  const togglerMarcoPolo = () => {
+    marcoPolo === "Marco" ? setMarcoPolo("Polo") : setMarcoPolo("Marco");
+    marcoPoloToggler ==="Polo" ? setMarcoPoloToggler("Marco") : setMarcoPoloToggler("Polo");
+  };
+
   return (
     <div id="main">
-
+      <h1 id='marco-polo'>{marcoPolo}</h1>
+      <button id="marco-polp-toggler" onClick={togglerMarcoPolo}>{marcoPoloToggler}</button>
     </div>
   )
 }
